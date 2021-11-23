21254ed7f402528805679b5e106c90659ea8d366
justincannotcode <90057713+justincannotcode@users.noreply.github.com>
2021-11-22 22:22:06 -0500

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,4 +1,14 @@
 import os
 from flask import Flask, flash, redirect, render_template, request, session
 
+#homepage
+#log in
+  #register
+#figure out post log in page (profile?)
+#profile
+#browse
+  #listings
+  #buy
+#sell
+
 db = SQL("sqlite://everything.db")
