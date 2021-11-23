3e4d69d93c94700ec585fe1004c9347a0298bf91
justincannotcode <90057713+justincannotcode@users.noreply.github.com>
2021-11-22 22:22:30 -0500

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask, flash, redirect, render_template, request, session
 
-#homepage
+#pre login
 #log in
   #register
-#figure out post log in page (profile?)
+#figure out post log in page (#homepage)
 #profile
 #browse
   #listings
